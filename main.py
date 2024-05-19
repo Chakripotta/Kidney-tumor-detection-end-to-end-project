63e4608e4881cb3091e567f0252c03c27110a702
Chakripotta <pottachakri@gmail.com>
2024-05-19 15:55:32 +0530

    Updated workflows 19 05 2024

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_injestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom log")
+
+
+
+
+STAGE_NAME="Data Injestion stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
